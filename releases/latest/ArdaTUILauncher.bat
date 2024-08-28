997f49f9a9cb22ce70db1816ca16339d327f4001
ArdaT <127962996+MaymunAb@users.noreply.github.com>
2024-08-28 11:12:24 +0300

    Delete releases/latest directory

--- releases/latest/ArdaTUILauncher.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-openfiles >nul 2>&1
-if '%errorlevel%' neq '0' (
-    echo [ARDAT-UI][INFO]:Requesting Admin privileges
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
-)
-cd /d "%~dp0"
-powershell -ExecutionPolicy Bypass -File "UI.ps1" -LaunchUICode "9Gj9Gjxxx4RFRGI38"
